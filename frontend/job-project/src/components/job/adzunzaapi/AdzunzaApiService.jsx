03f76791864b62c02ddbc4bb19a5521e637d9acb
jylee760 <125090400+jylee760@users.noreply.github.com>
2024-07-27 12:57:48 -0400

    Update AdzunzaApiService.jsx

--- frontend/job-project/src/components/job/adzunzaapi/AdzunzaApiService.jsx
+++ frontend/job-project/src/components/job/adzunzaapi/AdzunzaApiService.jsx
@@ -2,7 +2,7 @@ import axios from 'axios'
 export async function retrieveJobs(jobType,location,searchField,distanceFilter){
     let searchContent = searchField.replace(/ /g, '%20');
     const contentType = `&content-type=application/json`;
-    const link = `/api/v1/api/jobs/us/search/1?app_id=a93ec7ad&app_key=9a62d01687d7f82a19bbafe6e0e77d67&results_per_page=20&where=${location}&distance=${distanceFilter}&title_only=${searchContent}`;
+    const link = `/api/v1/api/jobs/us/search/1?app_id=a93ec7ad&app_key={api_key}&results_per_page=20&where=${location}&distance=${distanceFilter}&title_only=${searchContent}`;
     let job=""
     if(jobType!==null){
         job = `&${jobType}=1`
